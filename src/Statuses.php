0c48de82c1d5145a6bbdf4fe8ada3072b4466809
Remco Tolsma <info@remcotolsma.nl>
2014-07-17 11:08:03 +0200

    Added statuses class and test.

---
+++ src/Statuses.php
@@ -0,0 +1,71 @@
+<?php
+
+/**
+ * Title: Mollie iDEAL statuses constants
+ * Description:
+ * Copyright: Copyright (c) 2005 - 2014
+ * Company: Pronamic
+ * @author Remco Tolsma
+ * @version 1.0.0
+ * @see https://www.mollie.nl/support/documentatie/betaaldiensten/ideal/en/
+ */
+class Pronamic_WP_Pay_Gateways_Mollie_IDeal_Statuses {
+	/**
+	 * Success
+	 *
+	 * @var string
+	 */
+	const SUCCESS = 'Success';
+
+	/**
+	 * Cancelled
+	 *
+	 * @var string
+	 */
+	const CANCELLED = 'Cancelled';
+
+	/**
+	 * Expired
+	 *
+	 * @var string
+	 */
+	const EXPIRED = 'Expired';
+
+	/**
+	 * Failure
+	 *
+	 * @var string
+	 */
+	const FAILURE = 'Failure';
+
+	/**
+	 * Checked before
+	 *
+	 * @var string
+	 */
+	const CHECKED_BEFORE = 'CheckedBefore';
+
+	/////////////////////////////////////////////////
+
+	/**
+	 * Transform an Mollie state to an more global status
+	 *
+	 * @param string $status
+	 */
+	public static function transform( $status ) {
+		switch ( $status ) {
+			case self::SUCCESS :
+				return Pronamic_WP_Pay_Statuses::SUCCESS;
+			case self::CANCELLED :
+				return Pronamic_WP_Pay_Statuses::CANCELLED;
+			case self::EXPIRED :
+				return Pronamic_WP_Pay_Statuses::EXPIRED;
+			case self::FAILURE :
+				return Pronamic_WP_Pay_Statuses::FAILURE;
+			case self::CHECKED_BEFORE :
+				return null;
+			default:
+				return null;
+		}
+	}
+}
---
+++ tests/StatusesTest.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * Title: Mollie iDEAL statuses constants tests
+ * Description:
+ * Copyright: Copyright (c) 2005 - 2014
+ * Company: Pronamic
+ * @author Remco Tolsma
+ * @version 1.0.0
+ * @see https://www.mollie.nl/support/documentatie/betaaldiensten/ideal/en/
+ */
+class Pronamic_WP_Pay_Gateways_Mollie_IDeal_StatusesTest extends PHPUnit_Framework_TestCase {
+	/**
+	 * @dataProvider status_matrix_provider
+	 */
+	public function test_transform( $mollie_status, $expected ) {
+		$status = Pronamic_WP_Pay_Gateways_Mollie_IDeal_Statuses::transform( $mollie_status );
+
+		$this->assertEquals( $expected, $status );
+	}
+
+	public function status_matrix_provider() {
+		return array(
+			array( Pronamic_WP_Pay_Gateways_Mollie_IDeal_Statuses::SUCCESS, Pronamic_WP_Pay_Statuses::SUCCESS ),
+			array( Pronamic_WP_Pay_Gateways_Mollie_IDeal_Statuses::CANCELLED, Pronamic_WP_Pay_Statuses::CANCELLED ),
+			array( Pronamic_WP_Pay_Gateways_Mollie_IDeal_Statuses::EXPIRED, Pronamic_WP_Pay_Statuses::EXPIRED ),
+			array( Pronamic_WP_Pay_Gateways_Mollie_IDeal_Statuses::FAILURE, Pronamic_WP_Pay_Statuses::FAILURE ),
+			array( Pronamic_WP_Pay_Gateways_Mollie_IDeal_Statuses::CHECKED_BEFORE, null ),
+			array( 'not existing status', null ),
+		);
+    }
+}
