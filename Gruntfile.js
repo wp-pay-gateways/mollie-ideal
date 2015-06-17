43f7ef676b3bfdb5a5a591a16e47f68961ebdaa4
Remco Tolsma <info@remcotolsma.nl>
2015-06-17 15:41:35 +0200

    Added Grunt.

---
+++ Gruntfile.js
@@ -0,0 +1,63 @@
+module.exports = function( grunt ) {
+	// Project configuration.
+	grunt.initConfig( {
+		// Package
+		pkg: grunt.file.readJSON( 'package.json' ),
+
+		// JSHint
+		jshint: {
+			all: [ 'Gruntfile.js', 'composer.json', 'package.json' ]
+		},
+
+		// PHP Code Sniffer
+		phpcs: {
+			application: {
+				src: [
+					'**/*.php',
+					'!node_modules/**',
+					'!vendor/**'
+				 ],
+			},
+			options: {
+				standard: 'phpcs.ruleset.xml',
+				showSniffCodes: true
+			}
+		},
+
+		// PHPLint
+		phplint: {
+			options: {
+				phpArgs: {
+					'-lf': null
+				}
+			},
+			all: [ 'src/**/*.php' ]
+		},
+
+		// PHP Mess Detector
+		phpmd: {
+			application: {
+				dir: 'src'
+			},
+			options: {
+				exclude: 'node_modules',
+				reportFormat: 'xml',
+				rulesets: 'phpmd.ruleset.xml'
+			}
+		},
+		
+		// PHPUnit
+		phpunit: {
+			application: {},
+		},
+	} );
+
+	grunt.loadNpmTasks( 'grunt-contrib-jshint' );
+	grunt.loadNpmTasks( 'grunt-phpcs' );
+	grunt.loadNpmTasks( 'grunt-phplint' );
+	grunt.loadNpmTasks( 'grunt-phpmd' );
+	grunt.loadNpmTasks( 'grunt-phpunit' );
+
+	// Default task(s).
+	grunt.registerTask( 'default', [ 'jshint', 'phplint', 'phpmd', 'phpcs', 'phpunit' ] );
+};
