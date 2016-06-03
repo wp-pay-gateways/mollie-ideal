4af5057c3c4ce6bd00ad946d5499383277e0451e
I <info@remcotolsma.nl>
2016-06-03 13:22:45 +0200

    Removed $data and $payment_method parameters, no longer required.

--- src/Gateway.php
+++ src/Gateway.php
@@ -103,14 +103,13 @@ class Pronamic_WP_Pay_Gateways_Mollie_IDeal_Gateway extends Pronamic_WP_Pay_Gate
 	/**
 	 * Start
 	 *
-	 * @param Pronamic_Pay_PaymentDataInterface $data
 	 * @see Pronamic_WP_Pay_Gateway::start()
 	 */
-	public function start( Pronamic_Pay_PaymentDataInterface $data, Pronamic_Pay_Payment $payment, $payment_method = null ) {
+	public function start( Pronamic_Pay_Payment $payment ) {
 		$result = $this->client->create_payment(
-			$data->get_issuer_id(),
-			Pronamic_WP_Util::amount_to_cents( $data->get_amount() ),
-			$data->get_description(),
+			$payment->get_issuer(),
+			Pronamic_WP_Util::amount_to_cents( $payment->get_amount() ),
+			$payment->get_description(),
 			$payment->get_return_url(),
 			$payment->get_return_url()
 		);
